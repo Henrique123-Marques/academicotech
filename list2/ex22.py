60a14ad289b449ea978b2dee6972fdac9c2ec3d4
Henrique Marques Santos Silva <henriquemarquessantossilva@hotmail.com>
2024-05-27 22:04:28 -0300

    up_27_5_2024 return

--- list2/ex22.py
+++ list2/ex22.py
@@ -1,7 +1,8 @@
 class Ex22(object):
     """Crie um programa que receba 10 RMs e 10 notas de alunos e ao 
     final mostre o RA com a maior nota, o RM com a menor nota e a média das notas."""
-    def __init__(self, arg):
+    def __init__(self, rm, notas):
         super(Ex22, self).__init__()
-        self.arg = arg
+        self.rm = rm
+        self.notas = notas
         
