7aaf5a6ea948600c0f7aef151b848b4f3d63b320
Henrique Marques Santos Silva <henriquemarquessantossilva@hotmail.com>
2024-08-31 23:48:45 -0300

    update 21_ago

--- chatgpt_questions/ex1.py
+++ chatgpt_questions/ex1.py
@@ -1,16 +1,89 @@
-#Ex1 Escreva um programa que imprima todos os números pares de 1 a 50.
+class NúmerosPares:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que imprima todos os números pares de 1 a 50."
+        self.pares = []
+    	
+    def resolver(self):
+    	for i in range(1,51):
+    		if i % 2 == 0:
+    			self.pares.append(i)
+    	return self.pares
 
-class Ex1_gpt:
-	def __init__(self, arg):
-		self.arg = []
+obj1 = NúmerosPares()
+print(f'Ex1: Números pares de 1 a 50: {obj1.resolver()}\n')
 
-	def ParesFunc(self):
-		for i in range(1,50):
-			if i % 2 == 0:
-				self.arg.append(i)
-		return self.arg
+        
+class Tabuada:
+    def __init__(self):
+    	self.enunciado = "Escreva um programa que solicite um número do usuário e exiba a tabuada desse número de 1 a 10."
+    	self.num = float(input('Digite um numero: '))
 
+    def resolver(self):
+       for i in range(1,11):
+       		print(f'Resultado: {self.num} x {i} = {self.num * i}')
+obj2 = Tabuada()
+print(f'Ex2: {obj2.resolver()}\n')
 
-n_pares = Ex1_gpt([])
-print(f'{n_pares.ParesFunc()}')		
-		
+class SomaDeNúmeros:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que leia dois números do usuário e exiba a soma deles."
+    
+    def resolver(self):
+        # Adicione a solução aqui
+        pass
+
+class ConversãoDeTemperatura:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que converta uma temperatura de graus Celsius para Fahrenheit. A fórmula é: F = C * 9/5 + 32."
+    
+    def resolver(self):
+        # Adicione a solução aqui
+        pass
+
+class VerificaçãoDeAnoBissexto:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que solicite um ano do usuário e determine se esse ano é bissexto."
+    
+    def resolver(self):
+        # Adicione a solução aqui
+        pass
+
+class ContagemDeVogais:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que conte o número de vogais em uma string fornecida pelo usuário."
+    
+    def resolver(self):
+        # Adicione a solução aqui
+        pass
+
+class InverterString:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que inverta uma string fornecida pelo usuário."
+    
+    def resolver(self):
+        # Adicione a solução aqui
+        pass
+
+class Fatorial:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que calcule o fatorial de um número fornecido pelo usuário."
+    
+    def resolver(self):
+        # Adicione a solução aqui
+        pass
+
+class ContagemRegressiva:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que faça uma contagem regressiva de 10 até 0 e imprima 'Fogo!' ao final."
+    
+    def resolver(self):
+        # Adicione a solução aqui
+        pass
+
+class ParOuImpar:
+    def __init__(self):
+        self.enunciado = "Escreva um programa que solicite um número do usuário e informe se ele é par ou ímpar."
+    
+    def resolver(self):
+        # Adicione a solução aqui
+        pass
